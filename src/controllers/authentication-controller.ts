97834e8952f07639373c66a91a1f2032a5829a3d
kendy-karakawa <kendy.karakawa@gmail.com>
2023-06-03 19:13:31 -0300

    feat: sigin controller

---
+++ src/controllers/authentication-controller.ts
@@ -0,0 +1,14 @@
+import authenticationService from "@/services/authentication-service";
+import { Request, Response, NextFunction } from "express";
+import httpStatus from "http-status";
+
+export async function signIn (req: Request, res:Response, next:NextFunction) {
+    const {email, password} = req.body
+
+    try {
+        const result = await authenticationService.signIn(email, password)
+        return res.status(httpStatus.OK).send(result);
+    } catch (error) {
+        next(error)
+    }
+}
