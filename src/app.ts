ff8c5c4ffc6859456f5f1c2851e8e312dcfda9ba
kendy-karakawa <kendy.karakawa@gmail.com>
2023-06-03 19:19:35 -0300

    Feat: add authenrication router in app

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import express, { Express } from "express";
 import cors from "cors";
-
 import { loadEnv, connectDb, disconnectDB } from "@/config";
 import userRouter from "./routers/user-router";
 import { handleApplicationErrors } from "./middlewares";
+import authenticationRouter from "./routers/authentication-router";
 
 loadEnv();
 
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.get('/test', (_req, res) => res.send('OK!'))
 app.use('/user', userRouter)
+app.use('/sigin', authenticationRouter)
 app.use(handleApplicationErrors)
 
 export function init(): Promise<Express> {
--- src/routers/authentication-router.ts
+++ src/routers/authentication-router.ts
@@ -1,5 +1,8 @@
+import { signIn } from "@/controllers/authentication-controller";
 import { Router } from "express";
 
 const authenticationRouter = Router()
+
+authenticationRouter.post('/', signIn)
 
 export default authenticationRouter
