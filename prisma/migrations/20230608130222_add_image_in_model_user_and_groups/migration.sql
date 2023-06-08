16606f166b2880c51d0bc76e456ae2266580c43a
kendy-karakawa <kendy.karakawa@gmail.com>
2023-06-08 10:27:27 -0300

    add image line in model user/group

---
+++ prisma/migrations/20230608130222_add_image_in_model_user_and_groups/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Groups" ADD COLUMN     "image" TEXT NOT NULL DEFAULT E'https://cdn.pixabay.com/photo/2018/11/13/21/44/instagram-3814060_1280.png';
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "image" TEXT NOT NULL DEFAULT E'https://cdn-icons-png.flaticon.com/512/3106/3106921.png';
