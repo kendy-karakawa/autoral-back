8832f8fcbdc6926487ac47704c168e1e8581a6e0
kendy-karakawa <kendy.karakawa@gmail.com>
2023-06-26 21:15:39 -0300

    fix: complete debug in user router

--- src/controllers/authentication-controller.ts
+++ src/controllers/authentication-controller.ts
@@ -4,10 +4,8 @@ import httpStatus from "http-status";
 
 export async function signIn (req: Request, res:Response, next:NextFunction) {
     const {email, password} = req.body
-    console.log(`Entrei no controler e recebi email:${email}, senha: ${password} e vou para o service`)
     try {
         const result = await authenticationService.signIn(email, password)
-        console.log(`sai do service e recebi o result:${result} e vou responder com 201 e o result`)
         return res.status(httpStatus.OK).send(result);
     } catch (error) {
         next(error)
